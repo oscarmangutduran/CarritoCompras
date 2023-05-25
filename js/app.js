f3ccb7b70da62f90d84c81fa5d7538cf3dbdec7e
Oscar Mangut <mangutduranoscar@gmail.com>
2023-05-25 13:36:43 +0200

    Leer los datos que seleccionamos

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     // Dispara cuando se presiona "Agregar Carrito"
+     listaCursos.addEventListener('click', agregarCurso);
+
+}
+
+// Funciones
+
+function agregarCurso(evt) {
+     evt.preventDefault();
+     
+     if(evt.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = evt.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+     }
+}
+
+//Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso) {
+    console.log(curso)
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src, 
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
